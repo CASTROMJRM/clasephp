3d26a5751c8cf9185625cdfcf9048cd5bd784829
Juan_Manuel_Castro <20230008@uthh.edu.mx>
2024-06-19 08:03:38 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+echo "Mi primer uso php";
+echo "<br>";
+echo "Mis datos:";
+$nombre = "Castro";
+$edad="19";
+$estatura="1.76";
+echo " Soy...".strtoupper($nombre). " tengo ". $edad. " años y mido " .$estatura. "metros";
+echo "<br>";
+var_dump(is_int($nombre));
+echo "<br>";
+$x =34;
+$y =350;
+$suma=$x + $y;
+$resta=$x - $y;
+$multiplicacion=$x * $y;
+$division=$x / $y;
+echo "La suna es $x + $y  =$suma <br>";
+echo "La suna es $x + $y  =$resta <br>";
+echo "La suna es $x + $y  =$multiplicacion <br>";
+echo "La suna es $x + $y  =$division <br>";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prática php</title>
+</head>
+<body>
+   
+</body>
+</html>
